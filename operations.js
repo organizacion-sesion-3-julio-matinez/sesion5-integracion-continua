b49d98fe560f4483f214baf1ab68ed544f2fd819
Julio Martínez <j.martinez2@edu.gva.es>
2024-02-26 21:26:36 +0100

    Funcion par

---
+++ operations.js
@@ -0,0 +1,29 @@
+/* Función suma */
+var suma = function suma (a, b) {
+	return a + b;	
+};
+
+/* Función resta */
+var resta = function suma (a, b) {
+	return a - b;	
+};
+
+// Funcion par
+
+var par = function par (a) {
+	if (typeof(a) === 'string') {
+        return undefined;
+    }
+    if (a% 2 === 0) {
+        return "par";
+    }
+    return "impar";
+};
+
+/* Exportación de funciones */
+module.exports = {
+	suma,
+	resta,
+	par
+}
+
---
+++ test/test.js
@@ -0,0 +1,22 @@
+// Ejemplo de test utilizando el módulo assert disponible en NodeJS
+
+// Cargar el módulo assert
+var assert = require('assert');
+
+// Cargar el módulo con las funciones para testear
+var operaciones = require('../operations.js');
+
+// Test
+it('comprobar función suma', function() {
+	assert.equal(operaciones.suma(1,3), 4);
+	assert.equal(operaciones.suma(-1,3), 2);
+	assert.equal(operaciones.suma(8,-3), 5);
+});
+
+it('comprobar función par', function() {
+	assert.equal(operaciones.par(3), "impar");
+	assert.equal(operaciones.par(4), "par");
+	assert.equal(operaciones.par("hola"), undefined);
+});
+
+
